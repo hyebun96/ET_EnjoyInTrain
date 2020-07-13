35be9ac26588b0c26249088b51aff09640587958
hyebun <gpqns96@gmail.com>
2020-07-13 10:48:14 +0900

    faq 최종 업로드

--- enjoyInTrain/src/main/java/com/et/faq/FaQController.java
+++ enjoyInTrain/src/main/java/com/et/faq/FaQController.java
@@ -4,13 +4,10 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import javax.servlet.http.HttpSession;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller("faq.FaQController")
